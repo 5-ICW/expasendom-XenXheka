f7ea04a33d3a26df3d944024c2f292b4f3030f72
XenXheka <xen.xheka@student.tsaam.be>
2025-04-01 11:46:47 +0200

    versie1

--- script/script.js
+++ script/script.js
@@ -21,3 +21,34 @@ const users = [
 ];
 
 // hieronder komt jullie code - veel succes
+const name = document.getElementById("name");
+const userinput = getElementById("name");
+const email = document.getElementById("email");
+const filterTxt = document.getElementById("username");
+const password = document.getElementById("password");
+const regristreerBtn = document.getElementById("registreer");
+const user2 = document.getElementById("username");
+const user2Email = document.getElementById("email");
+const filterBtn = document.getElementById("filterForm");
+console.log(users);
+
+filterTxt.innerHTML = "";
+regristreerBtn.onclick = function () {
+  const newUser = {
+    fullname: name.value,
+    email: email.value,
+    password: password.value,
+  };
+  users.push(newUser);
+  console.log(newUser);
+};
+
+filterBtn.addEventListener("click", (e) => {
+  const filtered = users.filter((user) =>
+    user.fullname
+      .toLocaleLowerCase()
+      .includes(filterTxt.value.toLocaleLowerCase())
+  );
+  console.log(filtered);
+  filterTxt.append(filtered);
+});
